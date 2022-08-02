ed1ea375c9d89bec3dd950e8b98cd209fdc4e51d
Brad Pasley <bradpasley@catalyst-au.net>
2022-08-02 18:50:54 +1000

    GUI fixed separating modules - skip locks'

--- lib.php
+++ lib.php
@@ -1141,6 +1141,7 @@ function get_adhoctask_from_taskid(int $taskid) {
     foreach ($cdmadhoctasks as $adhoctask) {
         if ($adhoctask->get_id() == $taskid) {
             $thisadhoctask = $adhoctask;
+            break;
         }
     }
     if (isset($thisadhoctask)) {
