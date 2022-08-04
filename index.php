d4689158d36f30e62698f2a621bcdc4c15f1b685
Brad Pasley <bradpasley@catalyst-au.net>
2022-08-04 13:45:05 +1000

    Resolve PHPdoc check failures

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Main GUI page; displays reports and fix recommendations.
+ *
  * @package     tool_fix_delete_modules
  * @category    admin
  * @author      Brad Pasley <brad.pasley@catalyst-au.net>
